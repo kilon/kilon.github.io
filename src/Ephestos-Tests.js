29735b91684ff2f6f36be43bf799abb1cf1efa64
kilon <thekilon@yahoo.co.uk>
2014-06-28 14:41:26 +0300

    add amber init files

---
+++ src/Ephestos-Tests.js
@@ -0,0 +1,7 @@
+define("amber_core/Ephestos-Tests", ["amber_vm/smalltalk", "amber_vm/nil", "amber_vm/_st", "amber_vm/globals", "amber_core/SUnit"], function(smalltalk,nil,_st, globals){
+smalltalk.addPackage('Ephestos-Tests');
+smalltalk.packages["Ephestos-Tests"].transport = {"type":"amd","amdNamespace":"amber_core"};
+
+smalltalk.addClass('EphestosTest', globals.TestCase, [], 'Ephestos-Tests');
+
+});
---
+++ src/Ephestos.js
@@ -0,0 +1,7 @@
+define("amber_core/Ephestos", ["amber_vm/smalltalk", "amber_vm/nil", "amber_vm/_st", "amber_vm/globals", "amber_core/Kernel-Objects"], function(smalltalk,nil,_st, globals){
+smalltalk.addPackage('Ephestos');
+smalltalk.packages["Ephestos"].transport = {"type":"amd","amdNamespace":"amber_core"};
+
+smalltalk.addClass('Ephestos', globals.Object, [], 'Ephestos');
+
+});
